981e3b6738606d21c785a557c12e27f139067b29
denis909 <denis909@mail.ru>
2020-11-14 22:17:04 +0300

    n/a

--- FrontendController.php
+++ FrontendController.php
@@ -2,74 +2,9 @@
 
 namespace denis909\frontend;
 
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use Yii;
-use yii\helpers\Url;
-
-class FrontendController extends \yii\web\Controller
+class FrontendController extends \denis909\yii\Controller
 {
 
-    public $access = [];
-
     public $userComponent = 'user';
-
-    public $postActions = [];
-
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        $return = [
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => []
-            ],
-            'access' => array_merge(
-                [
-                    'class' => AccessControl::class,
-                    'user' => $this->userComponent,
-                    'rules' => [],
-                    'only' => []
-                ],
-                $this->access
-            )
-        ];
-
-        foreach($this->postActions as $action)
-        {
-            $return['verbs']['actions'][$action][] = 'POST';
-        }        
-
-        if (count($return['access']['rules']) == 0)
-        {
-            unset($return['access']);
-        }
-
-        if (count($return['verbs']['actions']) == 0)
-        {
-            unset($return['verbs']);
-        }
-
-        return $return;
-    }
-
-    protected function redirectBack($defaultUrl = null)
-    {
-        $returnUrl = Yii::$app->request->get('returnUrl');
-
-        if (!$returnUrl || !Url::isRelative($returnUrl))
-        {
-            $returnUrl = $defaultUrl;
-        }
-
-        if ($returnUrl === null)
-        {
-            $returnUrl = [$this->defaultAction];
-        }
-
-        return $this->redirect($returnUrl);
-    }
 
 }
